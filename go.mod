a574daf13e8b5479560c052749b355e93248473d
Wei Sun <swyddz1989@gmail.com>
2021-07-28 16:34:27 -0700

    Add more setups for recon services.

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.16
 require (
 	cloud.google.com/go v0.82.0
 	cloud.google.com/go/bigtable v1.10.1
+	github.com/google/go-cmp v0.5.6
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/api v0.47.0
 	google.golang.org/genproto v0.0.0-20210601144548-a796c710e9b6
 	google.golang.org/grpc v1.38.0
--- test/integration/resolve_entities_test.go
+++ test/integration/resolve_entities_test.go
@@ -1,7 +1,70 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package integration
 
-import "testing"
+import (
+	"context"
+	"path"
+	"runtime"
+	"testing"
+
+	pb "github.com/datacommonsorg/reconciliation/internal/proto"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
 
 func TestResolveEntities(t *testing.T) {
-	// TODO(spaceenter): Implement.
+	t.Parallel()
+	ctx := context.Background()
+	client, err := setup()
+	if err != nil {
+		t.Fatalf("Failed to set up recon client")
+	}
+	_, filename, _, _ := runtime.Caller(0)
+	goldenPath := path.Join(
+		path.Dir(filename), "golden_response/resolve_entities")
+
+	for _, c := range []struct {
+		req        *pb.ResolveEntitiesRequest
+		goldenFile string
+	}{
+		{
+			&pb.ResolveEntitiesRequest{},
+			"result.json",
+		},
+	} {
+		resp, err := client.ResolveEntities(ctx, c.req)
+		if err != nil {
+			t.Errorf("could not ResolveEntities: %s", err)
+			continue
+		}
+
+		if generateGolden {
+			updateProtoGolden(resp, goldenPath, c.goldenFile)
+			continue
+		}
+
+		var expected pb.ResolveEntitiesResponse
+		if err = readJSON(goldenPath, c.goldenFile, &expected); err != nil {
+			t.Errorf("Can not Unmarshal golden file")
+			continue
+		}
+
+		if diff := cmp.Diff(resp, &expected, protocmp.Transform()); diff != "" {
+			t.Errorf("payload got diff: %v", diff)
+			continue
+		}
+	}
 }
